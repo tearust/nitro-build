1f90267ea4864611bdb8092b88c8c30d7ff16d02
Yan Mingzhi <realraindust@gmail.com>
2021-03-30 16:05:53 +0800

    improve parent client script

--- parent-client.sh
+++ parent-client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Run parent-instance-client inside /code after docker container starts"
-docker run --network host -v /home/ec2-user:/code --rm -it ubuntu:20.10 /bin/sh
+docker run --network host -v /home/ec2-user:/code -w /code/nitro-build --rm -it ubuntu:20.10 /bin/bash
