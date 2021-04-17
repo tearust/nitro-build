ceb40ae168954ed4490969561e20daef33a8c918
Yan Mingzhi <realraindust@gmail.com>
2021-04-17 23:13:06 +0800

    update aws push command

--- client-docker.sh
+++ client-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Run parent-instance-client inside /code after docker container starts"
-docker run --network host -v /home/ec2-user:/code -w /code --rm -it ubuntu:20.10 /bin/bash
+docker run --name client-docker --network host -v /home/ec2-user:/code -w /code --rm -it ubuntu:20.10 /bin/bash
