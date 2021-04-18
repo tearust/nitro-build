ce0d8bb630c7f8322d5f6e82b9fc1706279913f6
Yan Mingzhi <realraindust@gmail.com>
2021-04-19 07:58:41 +0800

    fix adapter run account error

--- adapter.sh
+++ adapter.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+DOCKERHUB_ACCOUNT=$2
+: ${DOCKERHUB_ACCOUNT:="tearust"}
+
 if [ $1 = "run" ]; then
     docker run -d --network host --name adapter ${DOCKERHUB_ACCOUNT}/adapter:latest
 fi
