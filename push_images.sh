9c1a31804e09939e6455489cebaea4630f5aee03
Yan Mingzhi <realraindust@gmail.com>
2021-10-14 20:42:21 +0800

    untag after pushed images

--- push_images.sh
+++ push_images.sh
@@ -2,9 +2,12 @@
 
 docker tag tearust/runtime:nitro realraindust/runtime:latest
 docker push realraindust/runtime:latest
+docker rmi realraindust/runtime:latest
 
 docker tag tearust/vmh-server:nitro realraindust/vmh-server:latest
 docker push realraindust/vmh-server:latest
+docker rmi realraindust/vmh-server:latest
 
 docker tag tearust/parent-instance-client:latest realraindust/parent-instance-client:latest
 docker push realraindust/parent-instance-client:latest
+docker rmi realraindust/parent-instance-client:latest
