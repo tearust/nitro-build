f6cfbba28fa9b0b9c27e9249dbb56e31acdca99f
Yan Mingzhi <realraindust@gmail.com>
2022-10-04 17:28:51 +0800

    support run client through cli

--- client-docker.sh
+++ client-docker.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
-echo "Run parent-instance-client inside /code after docker container starts"
-docker run --name client-docker --network host -v /home/ec2-user:/code -w /code --rm -it ubuntu:20.10 /bin/bash
+
+docker run 
+	--name client 
+	--network host 
+	--rm 
+	-v .log:/log
+	-v .tokenstate:/tokenstate
+	-v .libp2p:/libp2p
+	-v .layer1:/layer1
+	-v ./genesis.json:/tearust/data/genesis.json
+	-e ENCLAVE_CID=6
+	-e LOG_FILE=/log/output.log
+  -e GENESIS_CONFIG_PATH=/tearust/data/genesis.json
+  -e LAYER1_WS_PROVIDER_URL=wss://goerli.infura.io/ws/v3/4926c6c0569842b89fb5df97a537e198
+  -e LAYER1_KEY_PATH=/layer1/key
+	-e TEA_ID="0xdf38cb4f12479041c8e8d238109ef2a150b017f382206e24fee932e637c2db7b"
+	-e MACHINE_OWNER="0x754d7b24d8a4ec00000000000000000000000000"
+	-e LIBP2P_NODE_KEY_PATH=/libp2p/key
+	-e LIBP2P_VERSION="local0.1"
+	-e IP_ADDRESS=127.0.0.1
+	-e APPLY_VALIDATOR="true"
+	-e STATE_MAGIC_NUMBER=100
+	-e PERSIST_PATH=/tokenstate/persist
+	-e RUST_BACKTRACE=full
+	-it tearust/parent-instance-client:nitro-cli /bin/bash
