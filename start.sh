7f3f47e27540ad78fa6ee77d4d07d77e3a149325
Yan Mingzhi <realraindust@gmail.com>
2023-03-25 11:30:33 +0800

    improve start script

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-RUN_MODE=$1
+TEA_ID=$1
+MACHINE_OWNER=$2
+AWS_REGION=$3
+RUN_MODE=$4
+: ${TEA_ID:=""}
+: ${MACHINE_OWNER:=""}
+: ${AWS_REGION:=""}
 : ${RUN_MODE:="debug"}
+
+set -e
+
+echo "begin to pre settings..."
+./pre-settings.sh $TEA_ID $MACHINE_OWNER $AWS_REGION
+echo "pre settings completed"
 
 echo "begin to start enclave runtime..."
 ./enclave.sh clean || true
