8d868cb3d3e623624d06e42be455fbcc9eba8b3f
Yan Mingzhi <realraindust@gmail.com>
2023-06-15 11:18:21 +0800

    update group id

--- aws-tool.sh
+++ aws-tool.sh
@@ -110,7 +110,7 @@ elif [ $1 = "create" ]; then
     SECURITY_GROUP_IDS=$4
     : ${IMAGE_ID:="ami-013218fccb68a90d4"}
     : ${KEY_NAME:="aws-tea-northeast2"}
-    : ${SECURITY_GROUP_IDS:="sg-03b0f4504627867f0"}
+    : ${SECURITY_GROUP_IDS:="sg-0649cb99605378ead"}
 
     aws ec2 run-instances \
         --image-id $IMAGE_ID \
