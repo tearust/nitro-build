66a388e8551e8fe8edc1c3efc87b3b05e31df6ac
Yan Mingzhi <realraindust@gmail.com>
2024-04-05 08:58:29 +0800

    update image version

--- enclave.sh
+++ enclave.sh
@@ -17,8 +17,8 @@ function console_print() {
 if [ $1 = "docker" ]; then
   DOCKER_USER=$2
   : ${DOCKER_USER:="tearust"}
-  docker rmi $DOCKER_USER/runtime:alpha-3.5
-  sudo nitro-cli build-enclave --docker-uri $DOCKER_USER/runtime:alpha-3.5 --output-file enclave_app.eif
+  docker rmi $DOCKER_USER/runtime:alpha-3.7
+  sudo nitro-cli build-enclave --docker-uri $DOCKER_USER/runtime:alpha-3.7 --output-file enclave_app.eif
   echo "current docker images:"
   docker images
 elif [ $1 = "debug" ]; then
--- start.sh
+++ start.sh
@@ -25,6 +25,6 @@ echo "start enclave runtime completed"
 
 echo "begin to start client..."
 docker-compose -f docker-compose-b.yaml down
-docker rmi tearust/parent-instance-client:alpha-3.5
+docker rmi tearust/parent-instance-client:alpha-3.7
 docker-compose -f docker-compose-b.yaml up -d
 echo "start client completed"
