e0ef88d40cc81f596172ed06c66a130181feb9ab
Yan Mingzhi <realraindust@gmail.com>
2024-04-08 08:31:23 +0800

    update docker image version

--- start.sh
+++ start.sh
@@ -23,6 +23,6 @@ echo "start enclave runtime completed"
 
 echo "begin to start client..."
 docker-compose -f docker-compose-b.yaml down
-docker rmi tearust/parent-instance-client:beta-3.1 || true
+docker rmi tearust/parent-instance-client:beta-4.0 || true
 docker-compose -f docker-compose-b.yaml up -d
 echo "start client completed"
